46a669ae3cdf729ed3641a43ef67942eccb2704f
ropering <bonoboss1028@gmail.com>
2022-02-05 23:03:31 +0900

    Print Humidity & Temperature to the Web

--- iot/main/views.py
+++ iot/main/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+# DHT11
+import time
+import board
+import adafruit_dht
+import psutil
 
 def index(request):
-    return HttpResponse("Helo world")
+    # We first check if a libgpiod process is running. If yes, we kill it!
+    for proc in psutil.process_iter():
+        if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+            proc.kill()
+
+    sensor = adafruit_dht.DHT11(board.D23)
+    try:
+        temp = sensor.temperature
+        humidity = sensor.humidity
+        print("Temperature: {}*C   Humidity: {}% ".format(temp, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+    except Exception as error:
+        sensor.exit()
+        raise error
+    context = {
+        'temp' : temp,
+        'hum' : humidity,
+    }
+    return render(request, 'main/index.html', context)
