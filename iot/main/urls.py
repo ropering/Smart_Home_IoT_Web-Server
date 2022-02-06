bd2a14730245fafba595ce2fb07d8a7cf9fb6003
ropering <bonoboss1028@gmail.com>
2022-02-06 18:22:15 +0900

    Ansynchronous Humidity & Temperature to the WEB

--- iot/main/urls.py
+++ iot/main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('get_value', views.get_value),
+    path('test', views.test),
+
 ]
--- iot/main/views.py
+++ iot/main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.shortcuts import render
 # DHT11
@@ -8,13 +8,15 @@ import board
 import adafruit_dht
 import psutil
 
+
 def index(request):
     # We first check if a libgpiod process is running. If yes, we kill it!
     for proc in psutil.process_iter():
         if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
             proc.kill()
-
+            
     sensor = adafruit_dht.DHT11(board.D23)
+
     try:
         temp = sensor.temperature
         humidity = sensor.humidity
@@ -24,8 +26,35 @@ def index(request):
     except Exception as error:
         sensor.exit()
         raise error
-    context = {
-        'temp' : temp,
-        'hum' : humidity,
-    }
-    return render(request, 'main/index.html', context)
+    else:
+        context = {
+            'temp' : temp,
+            'hum' : humidity,
+        }
+        return render(request, 'main/index.html', context)
+    return HttpResponse('<h1>다시 실행해주세요</h1>')
+
+def get_dht_data():
+    for proc in psutil.process_iter():
+        if proc.name() == 'libgpiod_pulsein' or proc.name() == 'libgpiod_pulsei':
+            proc.kill()
+    sensor = adafruit_dht.DHT11(board.D23)
+
+    try:
+        temp = sensor.temperature
+        hum = sensor.humidity
+        if temp is not None and hum is not None:
+            return temp, hum
+    except RuntimeError as err:
+        print('1', err.args[0])
+        return None, None
+    except Exception as err:
+        sensor.exit()
+        raise ('2', err)
+
+def get_value(request):
+    temp, hum = get_dht_data()
+    return JsonResponse({"temp": temp, "hum": hum})
+
+def test(request):
+    return JsonResponse({"text" : 'Hello world~'})
